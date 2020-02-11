05424b74519a88112f52459e551e914f89b91292
evojgani <vojgani@gwdg.de>
2020-02-11 14:24:59 +0100

    Update Phenotypepredictionfortopinteractions.R

--- pkg/R/Phenotypepredictionfortopinteractions.R
+++ pkg/R/Phenotypepredictionfortopinteractions.R
@@ -3,70 +3,102 @@
 #'
 #' @description Function to do phenotype prediction based on the desired proportion of pairwise SNP interactions
 #'
-#' @param pheno_train A subset of one numeric phenotype vector as a training set with names for each phenotypic value
-#' @param G_ERRBLUP ERRBLUP relationship matrix with row names and column names of all the individuals
+#' @param Pheno_train A subset of one numeric phenotype vector as a training set with names for each phenotypic value
 #' @param Gtop sERRBLUP Relationship matrix for the k percent of pairwise SNP interactions with row names and column names of all the individuals
+#' @param Trainset A vector of individuals which are in the training set
 #'
 #' @return A numeric vector of both phenotype estimations of training set and phenotype predictions of test set based on sERRBLUP method
 #'
 #' @examples
 #' library(BGLR)
 #' data(wheat)
-#' pheno <- wheat.Y[,1]
-#' pheno_train <- pheno[1:round(4*length(pheno)/5)]
+#' geno <- wheat.X
+#' t1 <- sample(1:ncol(geno), 20)
+#' t2 <- sample(1:ncol(geno), 20)
+#' y1 <- rowSums((geno[,t1]==2) * (geno[,t2]==2))
+#' t1 <- sample(1:ncol(geno), 20)
+#' t2 <- sample(1:ncol(geno), 20)
+#' y2 <- rowSums((geno[,t1]==2) * (geno[,t2]==0))
+#' t1 <- sample(1:ncol(geno), 20)
+#' t2 <- sample(1:ncol(geno), 20)
+#' y3 <- rowSums((geno[,t1]==0) * (geno[,t2]==2))
+#' t1 <- sample(1:ncol(geno), 20)
+#' t2 <- sample(1:ncol(geno), 20)
+#' y4 <- rowSums((geno[,t1]==0) * (geno[,t2]==0))
+#' y <- y1+y2+y3+y4
+#' pheno <- scale(y)
+#' names(pheno) <- names(wheat.Y[,1])
+#' N <- length(pheno)
+#' n <- 60
+#' test <- sample(1:N,n)
+#' training <- (1:N)[-test]
+#' pheno_train <- pheno[training]
 #' m <- Recodemarkers(wheat.X)
 #' rownames(m) <- names(pheno)
-#' G_ERRBLUP <- Gall(m)
-#' t_hat <- SNP_effect(m, pheno_train, G_ERRBLUP)
-#' sigma_hat <- SNP_var(m, pheno_train, t_hat)
+#' G_ERRBLUP <- Gall(m, cores=15)
+#' G <- G_ERRBLUP$G
+#' pi <- G_ERRBLUP$Pi
+#' Estimation <- SNP_effect_var(m, pheno_train, G, pi, training, cores=15)
+#' t_hat <- Estimation$effect
+#' sigma_hat <- Estimation$effectvar
 #' k <- 10
-#' Gtop_effect <- Gtop(m, pheno, t_hat, k)
-#' Gtop_var <- Gtop(m, pheno, sigma_hat, k)
-#' sERRBLUP_effect <- sERRBLUP(pheno_train, G_ERRBLUP, Gtop_effect)
-#' sERRBLUP_var <- sERRBLUP(pheno_train, G_ERRBLUP, Gtop_var)
+#' Gtop_effect <- Gtop(m, t_hat, k, cores=15)
+#' Gtop_var <- Gtop(m, sigma_hat, k, cores=15)
+#' sERRBLUP_effect <- sERRBLUP(pheno_train, Gtop_effect, training)
+#' sERRBLUP_var <- sERRBLUP(pheno_train, Gtop_var, training)
 #'
 #' @export
 #'
 
 
-sERRBLUP <- function(pheno_train, G_ERRBLUP, Gtop) {
+sERRBLUP <- function(Pheno_train, Gtop, Trainset) {
 
-  if(is.null(names(pheno_train))){
+  if(is.null(names(Pheno_train))){
 
     stop("The individuals are not named")
 
   } else {
 
-  Pheno <- pheno_train[stats::complete.cases(pheno_train)]
-  Phenosid <- data.frame(ID = names(pheno_train), observation = Pheno)
 
-  n <- dim(Gtop)[1]
-  Zz <- diag(n)
-  Xx <- matrix(1,n,ncol=1)
+    Pheno <- Pheno_train[stats::complete.cases(Pheno_train)]
+    Phenosid <- data.frame(ID = names(Pheno), observation = Pheno)
 
-  y <- Phenosid[,2]
-  ntrain <- length(y)
-  X <- Xx[1:ntrain]
-  Z <- Zz[1:ntrain,]
+    n <- dim(Gtop)[1]
+    Zz <- diag(n)
+    Xx <- matrix(1,n,ncol=1)
 
-  Ginv <- MASS::ginv(Gtop)
+    y <- Phenosid[,2]
+    ntrain <- length(y)
+    X <- Xx[Trainset]
+    Z <- Zz[Trainset,]
 
-  Gtrain <- G_ERRBLUP[rownames(G_ERRBLUP) %in% Phenosid[,1],colnames(G_ERRBLUP) %in% Phenosid[,1]]
+    Ginv <- MASS::ginv(Gtop)
 
-  abc <- EMMREML::emmreml(y=y, X=cbind(matrix(1, nrow = ntrain, ncol=1)), Z=diag(ntrain), K=Gtrain)
-  vare <- abc$Ve
-  varg <- abc$Vu
-  lambda <- vare/varg
+    Gtrain <- Gtop[Trainset,Trainset]
 
-  LHS <- rbind(cbind(crossprod(X),crossprod(X,Z)),cbind(crossprod(Z,X),crossprod(Z)+Ginv*lambda))
-  RHS <- rbind(crossprod(X,y),crossprod(Z,y))
-  MME <- MASS::ginv(LHS)%*%RHS
-  prediction <- MME[-1,]+MME[1,1]
+    abc <- EMMREML::emmreml(y=y, X=cbind(matrix(1, nrow = ntrain, ncol=1)), Z=diag(ntrain), K=Gtrain)
+    vare <- abc$Ve
+    varg <- abc$Vu
+    lambda <- vare/varg
 
-  return(prediction)
+    if(lambda<0.001){
+      lambda = 0.001
+    }
+    if(lambda>1000){
+      lambda = 1000
+    }
 
+    LHS <- rbind(cbind(crossprod(X),crossprod(X,Z)),cbind(crossprod(Z,X),crossprod(Z)+Ginv*lambda))
+    RHS <- rbind(crossprod(X,y),crossprod(Z,y))
+    MME <- MASS::ginv(LHS)%*%RHS
+    prediction <- MME[-1,]+MME[1,1]
+
+    return(prediction)
+
+  }
 }
-}
+
+
 
 
 
