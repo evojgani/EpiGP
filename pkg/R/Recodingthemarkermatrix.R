77cd42a293d9402baa9d1d316c4942696c3ba44b
evojgani <vojgani@gwdg.de>
2020-02-17 10:28:21 +0100

    Update Recodingthemarkermatrix.R

--- pkg/R/Recodingthemarkermatrix.R
+++ pkg/R/Recodingthemarkermatrix.R
@@ -1,11 +1,11 @@
 
 #' @title Marker Recoding Function
 #'
-#' @description Function to recode marker matrix to {0, 1, 2} coded marker matrix
+#' @description Function to recode marker matrix to {0, 2} or {0, 1, 2} coded marker matrix
 #'
 #' @param M A {-1, 0, 1} or {0, 1} or character coded marker matrix
 #'
-#' @return A {0, 1, 2} or {0, 2} coded marker matrix
+#' @return A {0, 1, 2} or {0, 2} coded marker matrix. The function reports how the recoding was implemented in the input marker matrix.
 #'
 #' @examples
 #' library(BGLR)
