c2609d797b4a6be917e4c32e76ca231a5c1534c7
evojgani <vojgani@gwdg.de>
2020-11-27 12:09:38 +0100

    Update data.R

--- pkg/R/data.R
+++ pkg/R/data.R
@@ -1,7 +1,8 @@
 
 
-#' @title Simulated wheat Phenotypes
+#' @title Simulated Wheat Phenotypes
 #'
 #' @description Simulated phenotypes from Wheat dataset ("BGLR" package) genotype by randomly selection of \code{80} SNPs from marker matrix and generation of the phenotypic trait based on all \code{4} possible genotype combinations  \code{{00, 02, 20, 22}} of \code{{0,2}} coded marlers resulting in purely epistatic trait with the heritability of \code{1}
 #' @format A Phenotypic vector of length \code{599}
 "Phenotype"
+
